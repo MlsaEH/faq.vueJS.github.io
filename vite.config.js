76b285d46e95edff5aa3ca1b7e11e032b55e0a3c
Manu <eh@mlsa.fr>
2023-04-05 09:58:42 +0200

    vue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+//import vue from '@vitejs/plugin-vue'
+import vue from './node_modules/@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
